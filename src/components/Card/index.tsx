22c8b2b272f76416e421fd8ec516e2ee78858fec
Yvan Brito <yvanbrito96@gmail.comm>
2022-05-17 12:55:18 -0300

    improving card property

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,14 @@
+import { useState } from "react";
 import Router from "next/router";
-import Image from "next/image";
 import { IPropertyMapped } from "../../pages/busca";
-import imagem2 from "../../assets/imovel2.webp";
 
 interface CardProps {
   property: IPropertyMapped;
 }
 
 export default function Card({ property }: CardProps) {
+  const [imgSelected, setImgSelected] = useState<number>(0);
+
   const clickSubmit = () => {
     Router.push(`/imovel/${property.id}`);
   };
@@ -15,13 +16,54 @@ export default function Card({ property }: CardProps) {
   return (
     <div onClick={clickSubmit} key={property.streetName} className="card">
       <div className="card-details">
-        <Image
-          className="imgCard"
-          src={imagem2}
-          alt=""
-          width={300}
-          height={200}
-        />
+        <div
+          style={{
+            backgroundImage: `url("/assets/${property.images[imgSelected]}")`,
+          }}
+          className="card-details-top"
+        >
+          <div className="interactables">
+            <div className="side">
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setImgSelected((oldState) =>
+                    oldState > 0 ? --oldState : oldState
+                  );
+                }}
+                className="side-controllers side-controllers-left"
+              ></div>
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setImgSelected((oldState) =>
+                    oldState < 4 && oldState < property.images.length - 1
+                      ? ++oldState
+                      : oldState
+                  );
+                }}
+                className="side-controllers side-controllers-right"
+              ></div>
+            </div>
+            <div>
+              {property.images.map(
+                (i, index) =>
+                  index < 5 && (
+                    <span
+                      key={index}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setImgSelected(index);
+                      }}
+                      className={
+                        index === imgSelected ? "circle-selected" : "circle"
+                      }
+                    ></span>
+                  )
+              )}
+            </div>
+          </div>
+        </div>
         <div className="card-details-bottom">
           <span className="property-type">{property.type}</span>
           <h3>{property.streetName}</h3>
